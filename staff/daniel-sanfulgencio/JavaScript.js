34516c3aeceb0c4da4322cac18f2a6d4c60aabea
Daniel Sanfulgencio de la Ossa <ds.delaossa@gmail.com>
2025-02-03 21:11:53 +0100

    Rename my code

--- staff/daniel-sanfulgencio/JavaScript.js
+++ staff/daniel-sanfulgencio/JavaScript.js
@@ -1,9 +1,7 @@
 function guessNumber() {
     var number = Math.floor(Math.random() * 10) + 1; 
-    var guess = prompt('¿En qué número crees que estoy pensando? (1-10)');
-
-     while (guess !== number) {
-        
+    while (guess !== number) {
+        var guess = prompt('¿En qué número crees que estoy pensando? (1-10)');  
         if (guess === null || guess === '') {
             alert('Por favor, ingresa un número.');
             continue; 
@@ -13,10 +11,10 @@ function guessNumber() {
 
         if (guess === number) {
             alert('¡Enhorabuena! Has adivinado el número.');
-            guess;
+            continue;
         } else {
             alert('Buuuh! Perdedor!');
-            guess;
+            continue;
         }
     }
 }
