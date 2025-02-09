984f4d299df23ca86c3499542e067e576b59f99c
Daniel Sanfulgencio de la Ossa <ds.delaossa@gmail.com>
2025-02-09 17:24:53 +0100

    Improved Hangman game #17

---
+++ staff/daniel-sanfulgencio/Hangman2.js
@@ -0,0 +1,59 @@
+//Rehacemos el código del HangMan pero más limpio y optimizado
+
+var word = 'charmander';  
+var guessedWordArray = Array(word.length).fill('-'); 
+var guessedWord = ''; 
+var lifes = 5; 
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+// Convertimos guessedWordArray a string
+function updateGuessedWord() { 
+    guessedWord = guessedWordArray.join(''); 
+}
+
+// Inicializamos el guessedWordArray respetando los espacios
+for (var i = 0; i < word.length; i++) { 
+    if (word[i] === ' ') guessedWordArray[i] = ' '; 
+}
+updateGuessedWord();
+
+// Validamos que la entrada sea una letra válida
+function validateInputLetter(letter) {
+    letter = letter.toLowerCase(); // Convertimos a minúscula directamente
+    if (letter.length !== 1 || !alphabet.includes(letter)) {
+        alert('Make sure you put a single letter');
+        return null;
+    }
+    return letter;
+}
+
+// Verificamos si la letra está en la palabra y actualizar guessedWordArray
+function checkLetterIncluded(letter) { 
+    if (word.includes(letter)) {
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] === letter) guessedWordArray[i] = letter; 
+        }
+    } else {
+        lifes--; 
+    }
+}
+
+alert('Welcome to the Hangman game, guess a word or die on the 5th attempt!');
+
+while (guessedWord !== word && lifes > 0) {
+    var guessedLetter = prompt(`This is all you know about the word so far: \n   ${guessedWord} \nYou have ${lifes} lifes`);
+    
+    if (guessedLetter === null) {
+        lifes = 0;
+        alert('Well, you lose it. BYE!');
+        break;
+    }
+    
+    var validatedLetter = validateInputLetter(guessedLetter);
+    if (validatedLetter) {
+        checkLetterIncluded(validatedLetter);
+        updateGuessedWord();
+    }
+}
+
+alert(lifes > 0 ? `Congratulations, you win! The word was: ${word}` : 'You are a LOOSER. You better go home.');
